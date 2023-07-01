ae1730400edaf9e3338bbe6646d7ae700bef0199
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-07-01 17:52:29 +0300

    RemoteDictionaryServerBasicConsoleApp

---
+++ RedisConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using StackExchange.Redis; // Redis bağlantısı için StackExchange.Redis kütüphanesini kullanıyoruz
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string redisBaglanti = "localhost:6379";
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisBaglanti);
+        IDatabase database = redis.GetDatabase();
+
+        Dictionary<string, string> keyValue = new Dictionary<string, string>()
+        {
+            {"Name", "Ömer" },
+            {"Surname", "DİRİCANLI" },
+            {"Age", "29" }
+
+        };
+        foreach(var kv  in keyValue)
+        {
+            database.StringSet(kv.Key, kv.Value);
+        }
+
+        // Bir verileri arraya ekleme
+        List<RedisValue> DonenDeger = new List<RedisValue>();
+        foreach(var key in keyValue.Keys)
+        {
+            RedisValue value = database.StringGet(key);
+            DonenDeger.Add(value);
+        };
+       //Ekrana Yazdırma
+
+        for(int i = 0; i < keyValue.Count; i++)
+        {
+            Console.WriteLine("Keys değeri; " + keyValue.Keys.ElementAt(i) + "  Value Değeri: " + DonenDeger[i]) ;
+        }
+
+        redis.Close();
+
+       Console.ReadLine();
+    }
+}
